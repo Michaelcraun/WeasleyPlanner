09e97e34598228b7f14036ff768bd3f7fdf8927e
Michael Craun <michaelcraun@michaels-macbook-air.local>
2018-02-13 10:23:51 -0500

    - When user terminates app, their status is now set to false (away)

--- Weasley Planner/Extension/FoundationExtension.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FoundationExtension.swift
-//  Weasley Planner
-//
-//  Created by Michael Craun on 2/13/18.
-//  Copyright © 2018 Craunic Productions. All rights reserved.
-//
-
-import Foundation
